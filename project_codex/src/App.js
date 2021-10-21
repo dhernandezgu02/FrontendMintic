d31a77ad62b1557e7719c1dc50393e4fb5d42307
Daniel <danielmv92@gmail.com>
2021-10-20 23:20:05 -0500

    menu.js y menu.css

--- project_codex/src/App.js
+++ project_codex/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css"
 import Login from "./views/login/Login"
 import Update from "./views/update_employee/Update.js";
+import Menu from "./views/menu/Menu.js";
 import { Route, BrowserRouter as Router } from 'react-router-dom';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" component="" />
         <Route path="/Login" component={Login} />
         <Route path="/Update" component={Update} />
+        <Route path="/Menu" component={Menu} />
       </div>
     </Router>
   );
---
+++ project_codex/src/views/menu/Menu.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./menu.css"
+const Menu = () => {
+    return (
+        <div className="container">
+            <button className="btn">Ventas</button>
+            <button className="btn">Productos</button>
+            <button className="btn">Usuarios</button>
+        </div>
+    )
+
+}
+
+export default Menu;
